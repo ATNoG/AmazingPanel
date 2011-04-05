1f5ac0dd833790b047cfbf78794cbda0a6840103
João Martins <jmartins@av.it.pt>
2011-04-05 02:32:22 +0100

    Homepage first version
    - Layout
    - News Aggregator using config::NEWS_SOURCES
    * Missing:
    - Auto fetch all images at testbed/<id>/*.png

--- amazing-panel/app/controllers/pages_controller.rb
+++ amazing-panel/app/controllers/pages_controller.rb
@@ -3,7 +3,21 @@ class PagesController < ApplicationController
   respond_to :js, :only => [:application] 
   respond_to :css, :only => [:custom]
   
-  def index
+  def index 
+    @news = Array.new()
+    NEWS_SOURCES.each do |source|
+      url = URI.parse(source)
+      p = Net::HTTP::Get.new(url.path) 
+      http = Net::HTTP.new(url.host, url.port)
+      body = http.request(p).body
+      content = ActiveSupport::JSON.decode(body)
+      unless content.blank?
+        ns = content.take(5).uniq().collect! {|n| 
+          { :title => n['title'], :created_on => n['created_on'] }
+        }
+        @news = @news.concat(ns)
+      end
+    end
   end
   
   def application
--- amazing-panel/config/initializers/config.rb
+++ amazing-panel/config/initializers/config.rb
@@ -1,1 +1,6 @@
 APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
+
+NEWS_SOURCES = [
+  "http://helios.av.it.pt/projects/amazing-panel/news.json",
+  "http://mytestbed.net/news.json"
+]
